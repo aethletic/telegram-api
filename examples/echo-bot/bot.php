5e48072c7e5854a5923092ca3923607e98c6914e
aethletic <gorshkov34@gmail.com>
2021-01-10 15:52:28 +0400

    Update bot.php

--- examples/echo-bot/bot.php
+++ examples/echo-bot/bot.php
@@ -5,11 +5,10 @@ require __DIR__ . '/../../vendor/autoload.php';
 // Init our bot
 $bot = bot('1234567890:ABC_TOKEN');
 
-// Use long polling instead webhook
-$bot->longpoll(function (\Telegram\Bot $bot) {
-    // Catch all `message` and `edit_message`, because only they have `*.text` key.
-    $bot->on('*', function () {
-        // And now, get come `text` from update and send to user
-        say(update('*.text', 'default answer if *.text not exists'));
-    });
+// Catch all `message` and `edit_message`, because only they have `*.text` key.
+$bot->on('*', function () {
+    // And now, get come `text` from update and send to user
+    say(update('*.text', 'default answer if *.text not exists'));
 });
+
+$bot->run();
